0a5de264c3da68d0bb37be1ed73bc092bfb3c73c
Felipe Dias <adfelipealmeida@gmail.com>
2022-01-11 07:29:37 -0300

    add info email

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,12 @@ class MyCard extends StatelessWidget {
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
             child: Column(
-          children: const [
-            CircleAvatar(
+          children: [
+            const CircleAvatar(
               backgroundImage: AssetImage('imagens/bonecos.jpg'),
               radius: 50.0,
             ),
-            Text(
+            const Text(
               'Felipe Dias',
               style: TextStyle(
                 fontSize: 40.0,
@@ -28,13 +28,65 @@ class MyCard extends StatelessWidget {
                 fontFamily: 'Pacifico',
               ),
             ),
-            Text(
+            const Text(
               "Desenvolvedor Flutter",
               style: TextStyle(
                 fontSize: 20.0,
                 fontFamily: "Source Sans Pro",
                 color: Colors.white,
-                letterSpacing: 2.5,
+                letterSpacing: 2.5, // Aumenta a distância entre as letras
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.white,
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.phone,
+                    size: 25.0,
+                    color: Colors.deepOrange,
+                  ),
+                  SizedBox(width: 10.0), // Dar espaço entre os Widgets
+                  Text(
+                    "+55 14 99710-1478",
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontSize: 20.0,
+                      fontFamily: "Source Sans Pro",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.white,
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.email,
+                    size: 25.0,
+                    color: Colors.deepOrange,
+                  ),
+                  SizedBox(width: 10.0), // Dar espaço entre os Widgets
+                  Text(
+                    "adfelipealmeida@gmail.com",
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontSize: 20.0,
+                      fontFamily: "Source Sans Pro",
+                    ),
+                  ),
+                ],
               ),
             )
           ],
